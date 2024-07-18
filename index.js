57da913efac2724c69fdd7640dd2813e6f16d66e
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-18 19:25:22 +0700

    up

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ require("dotenv").config();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const postsRouter = require("./models/Posts");
-// const authRouter = require("./routes/auth.router");
-
-app.use("/api/v1/posts", postsRouter);
-// app.use("/api/v1/auth", authRouter);
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server is running....");
 });
+
+//
+//
 // const express = require("express");
 // const cors = require("cors");
 // const app = express();
