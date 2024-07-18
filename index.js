a5aef68d3580b67923b561aa3d9a694ef29ad65c
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-18 16:58:39 +0700

    up

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+// const express = require("express");
+// const app = express();
+// const db = require("./models");
+
+// require("dotenv").config();
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// // Routers
+// const postRouter = require("./routes/Posts");
+// app.use("/posts", postRouter);
+
+// const PORT = process.env.PORT || 3001;
+
+// db.sequelize.sync().then(() => {
+//   app.listen(PORT, () => {
+//     console.log("Server is running....");
+//   });
+// });
+
 const express = require("express");
 const app = express();
 const db = require("./models");
-
 require("dotenv").config();
 
 app.use(express.urlencoded({ extended: false }));
@@ -13,8 +33,13 @@ app.use("/posts", postRouter);
 
 const PORT = process.env.PORT || 3001;
 
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log("Server is running....");
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
   });
-});
--- models/Posts.js
+++ models/Posts.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define("Posts", {
+  const Post = sequelize.define("Post", {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
