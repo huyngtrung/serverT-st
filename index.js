3f90862c2cf35e03687b608258749ec296d3030f
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-18 12:31:01 +0700

    up

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
-const db = require("./models");
+
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+// const postsRouter = require("./routes/posts.router");
+// const authRouter = require("./routes/auth.router");
+
+// app.use("/api/v1/posts", postsRouter);
+// app.use("/api/v1/auth", authRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is running....");
 });
