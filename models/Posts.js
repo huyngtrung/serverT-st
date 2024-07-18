795796e2cc2aeca068ec75c101b087aa2ab548fe
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-18 16:36:48 +0700

    up

--- models/Posts.js
+++ models/Posts.js
@@ -1,9 +1,9 @@
-// module.exports = (sequelize, DataTypes) => {
-//   const Post = sequelize.define("Posts", {
-//     title: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//   });
-//   return Post;
-// };
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define("Posts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Post;
+};
