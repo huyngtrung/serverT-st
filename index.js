aa6a8c7a1c9c0be0a2c802cfcb52667c90b505ce
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-18 19:22:35 +0700

    up

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 
-app.use(cors());
+require("dotenv").config();
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Routers
-const postRouter = require("./routes/Posts");
-app.use("/posts", postRouter);
+const postsRouter = require("./models/Posts");
+// const authRouter = require("./routes/auth.router");
 
-const PORT = process.env.PORT || 3001;
+app.use("/api/v1/posts", postsRouter);
+// app.use("/api/v1/auth", authRouter);
 
-const db = require("./models");
-db.sequelize
-  .sync()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is running....");
+});
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+
+// app.use(cors());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// // Routers
+// const postRouter = require("./routes/Posts");
+// app.use("/posts", postRouter);
+
+// const PORT = process.env.PORT || 3001;
+
+// const db = require("./models");
+// db.sequelize
+//   .sync()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("Unable to connect to the database:", err);
+//   });
