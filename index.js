01745754b879d5af79d2f0ddf3aca3ae91197de1
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-19 16:58:00 +0700

    up

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
+const db = require("./models");
+const http = require("http");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -13,12 +15,12 @@ const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
 
 const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
 
-const db = require("./models");
 // db.sequelize
 //   .sync()
 //   .then(() => {
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 // })
