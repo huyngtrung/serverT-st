f01685e8245284111782af7f2d1eea9eb36d562d
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-19 17:05:28 +0700

    up

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 console.log(PORT);
 
-// db.sequelize
-//   .sync()
-//   .then(() => {
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-// })
-// .catch((err) => {
-//   console.error("Unable to connect to the database:", err);
-// });
+db.sequelize
+  .sync()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
@@ -13,13 +14,10 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_DBNAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: config.dialect,
-    }
+    config.database,
+    config.username,
+    config.password,
+    config
   );
 }
 
